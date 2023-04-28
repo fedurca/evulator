eaffc26945e218918a4f8629698113cb0aff55ac
Ludek Fedurca <fedurca@gmail.com>
2023-04-28 11:42:44 +0200

    clonescript add

---
+++ create_CD.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -
+cd cd
+git clone https://github.com/Jardito359/bezdratova-nabijecka
+git clone https://github.com/Rick538/fitness-tracker
+git clone https://github.com/Sky3R/projekt/
+git clone https://github.com/gabrielmaxh/Maturita
+git clone https://github.com/MeivrCZ/BeHiveV2Server
+git clone https://github.com/Hurbixcz/Smart-cup/blob/main/Smartcup..m
+git clone https://github.com/Tomasxdc
+git clone https://github.com/Zoltan4856/Metepstanice.git
+git clone https://github.com/Jirka135/Auto-s-docking-station
+git clone https://github.com/memoritale/maturita-projekt/tree/main
+git clone https://github.com/LukasStevula/Chytr--miska
+git clone https://github.com/KingPinos/Smart_desinfekce
+git clone https://github.com/PanDomaci/Smart_Garden_M5/blob/main/SmartGarden1.5.py
